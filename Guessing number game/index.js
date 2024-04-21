37fcb3bc2af30d50bea5f4a3b4d72e0c11c71b9b
Sufyan <msuffiyanrashid@gmail.com>
2024-04-20 22:22:49 -0700

    Add Number Guessing file

---
+++ Guessing number game/index.js
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+console.log(chalk.yellowBright("\n\t***   Welcome to CLI Number Guessing Game   ***\n"));
+const randomNum = Math.floor(Math.random() * 6 + 1);
+const attempt = await inquirer.prompt([{
+        name: "GuessedNum",
+        type: "number",
+        message: chalk.magenta("Enter your guess number from 1 to 6:")
+    }]);
+if (attempt.GuessedNum === randomNum) {
+    console.log(chalk.green("Congratulations! You guess a right number."));
+}
+else {
+    console.log(chalk.red("OOPS!") + " You are wrong. The correct answer is " + randomNum + ".\n");
+}
